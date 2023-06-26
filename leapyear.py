5b178edb1573d22747518f270b248fc5fc04943c
beaferreira <triszferreira2@gmail.com>
2023-06-26 18:48:31 -0300

    mais correcao

--- leapyear.py
+++ leapyear.py
@@ -4,8 +4,10 @@ year = int(input("Which year do you want to check? "))
 if (year%4==0):
     if (year%100==0):
         if (year%400==0):
-            print("Leap year")
-        else:
+            print("Leap year.")
+        else: 
             print("Not leap.")
-else: 
+    else:
+        print("Not leap.")
+else:
     print("Not leap.")
